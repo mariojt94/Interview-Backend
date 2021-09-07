87c715e76efaac45a3c34e8d5e3f9d79c8fbb1bc
mariojt94 <79564124+mariojt94@users.noreply.github.com>
2021-09-07 18:46:32 +0700

    commit

---
+++ Moduit.Interview/Moduit.Interview/Controllers/QuestionController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moduit.Interview.ViewModel;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Web.Http;
+using HttpGetAttribute = Microsoft.AspNetCore.Mvc.HttpGetAttribute;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
+using JsonFlatten;
+using Newtonsoft.Json;
+
+namespace Moduit.Interview.Controllers
+{
+    public class QuestionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        const string url1 = "https://screening.moduit.id/backend/question/one";
+        const string url2 = "https://screening.moduit.id/backend/question/two";
+        const string url3 = "https://screening.moduit.id/backend/question/three";
+
+        Question1ViewModel data = new Question1ViewModel();
+        List<Question2ViewModel> data2 = new List<Question2ViewModel>();
+        IQueryable<Question2ViewModel> data3 = null;
+
+        [Route("api/question/GetQuestion1")]
+        [HttpGet]
+        public Question1ViewModel GetQueSstion1()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(url1);
+
+            HttpResponseMessage response = client.GetAsync("").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                data = response.Content.ReadAsAsync<Question1ViewModel>().Result;  
+            }
+            return data;
+        }
+
+
+        [Route("api/question/GetQuestion2")]
+        [HttpGet]
+        public IQueryable<Question2ViewModel> GetQueSstion2()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(url2);
+
+            HttpResponseMessage response = client.GetAsync("").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                data2 = response.Content.ReadAsAsync<List<Question2ViewModel>>().Result;
+            }
+            var test = data2.Where(x => x.description.Contains("Ergonomic") || x.title.Contains("Ergonomic") || (x.tags?.Contains("Sports") ?? false)).OrderByDescending(x => x.id).Take(3).Reverse().ToList().AsQueryable();
+            return test;
+        }
+
+        [Route("api/question/GetQuestion3")]
+        [HttpGet]
+        public IQueryable<Question2ViewModel> GetQueSstion3()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(url3);
+
+            HttpResponseMessage response = client.GetAsync("").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                data3 = response.Content.ReadAsAsync<List<Question2ViewModel>>().Result.AsQueryable();
+            }
+
+            foreach (var item in data3)
+            {
+
+            }
+
+
+            return data3;
+        }
+
+    }
+}
---
+++ Moduit.Interview/Moduit.Interview/ViewModel/Question2ViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Moduit.Interview.ViewModel
+{
+    public class Question2ViewModel
+    {
+        public string id { get; set; }
+        public string category { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public string footer { get; set; }
+        public string[]? tags { get; set; }
+        public DateTime createdAt { get; set; }
+    }
+}
